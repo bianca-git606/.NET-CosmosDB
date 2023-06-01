800342f17a5c5d3319345055c0a84501f0b18379
biancagit-606 <biancafiedalan31@yahoo.com>
2023-06-01 14:25:37 +1200

    added a product file

---
+++ Product.cs
@@ -0,0 +1,13 @@
+public record Product (
+    string Id,
+    string CategoryId
+): Item (
+    Id, 
+    CategoryId, 
+    nameof(Product)
+) {
+    public string Name { get; init; } = default!;
+    public decimal Price { get; init; }
+    public bool Archived { get; init; }
+    public int Quantity { get; init; }  
+}
